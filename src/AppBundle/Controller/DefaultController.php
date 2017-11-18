740914dc4fd70e747ae2bbb8645e7b3707f40fe8
Andrii Popov <andrew.p@codesushi.co>
2017-11-18 11:56:30 +0000

    contact form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Feedback;
+use AppBundle\Form\FeedbackType;
 
 class DefaultController extends Controller
 {
@@ -13,7 +15,7 @@ class DefaultController extends Controller
      * @Route("/", name="homepage")
      * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $name = 'Bob';
         
@@ -26,8 +28,22 @@ class DefaultController extends Controller
      */
     public function feedbackAction(Request $request)
     {
-        $form = 'Here will be the form';
+        $feedback = new Feedback();
+        $feedback->setIpAddress($request->getClientIp());
+        $form = $this->createForm(FeedbackType::class, $feedback);
+        $form->handleRequest($request);
         
-        return ['form' => $form];
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+        
+            $em->persist($feedback);
+            $em->flush();
+            
+            $this->addFlash('fail', 'WRONG');
+            $this->addFlash('success', 'Message saved');
+            return $this->redirectToRoute('feedback'); // RedirectResponse
+        }
+        
+        return ['feedback_form' => $form->createView()];
     }
 }
---
+++ src/AppBundle/Entity/Feedback.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Feedback
+ *
+ * @ORM\Table(name="feedback")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FeedbackRepository")
+ */
+class Feedback
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ip_address", type="string", length=255)
+     */
+    private $ipAddress;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Feedback
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Feedback
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Feedback
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set ipAddress
+     *
+     * @param string $ipAddress
+     *
+     * @return Feedback
+     */
+    public function setIpAddress($ipAddress)
+    {
+        $this->ipAddress = $ipAddress;
+
+        return $this;
+    }
+
+    /**
+     * Get ipAddress
+     *
+     * @return string
+     */
+    public function getIpAddress()
+    {
+        return $this->ipAddress;
+    }
+}
+
