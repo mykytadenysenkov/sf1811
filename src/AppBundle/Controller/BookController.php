9e4f94962082038b4f166c5caee574cb6f85ceaf
Andrii Popov <andrew.p@codesushi.co>
2017-11-24 19:00:22 +0000

    api example

--- src/AppBundle/Controller/BookController.php
+++ src/AppBundle/Controller/BookController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use AppBundle\Entity\Book;
 
 class BookController extends Controller
@@ -40,6 +41,34 @@ class BookController extends Controller
         }
         
         return ['book' => $book];
+    }
+    
+    /**
+     * @Route("/api/books/{id}", name="book_item_api", requirements={"id":"[0-9]+"})
+     */
+    public function getBookApiAction($id)
+    {
+        // dz: get 500, 404 json if errors
+        $book = $this
+            ->getRepository()
+            ->find($id)
+        ;
+        
+        if (!$book) {
+            throw $this->createNotFoundException('D\'oh! Book not found');
+        }
+        
+        $data = [
+            'id' => $book->getId(),
+            'title' => $book->getTitle(),
+            'price' => $book->getPrice(),
+            'category' => [
+                'id' => $book->getCategory()->getId(),
+                'name' => $book->getCategory()->getName(),
+            ]
+        ];
+        
+        return new JsonResponse($data);
     }
     
     private function getRepository()
--- src/AppBundle/Resources/public/js/scripts.js
+++ src/AppBundle/Resources/public/js/scripts.js
@@ -1,3 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function($) {
+    $.get('/app_dev.php/api/books/1', function(r) {
+        console.log(r);
+    }); // app_dev - temporary
     console.log('Doc ready');
 });
